8b46fe16113bc7ab3c28016633025ca5d887aef3
LeonidGHA <104501564+LeonidGHA@users.noreply.github.com>
2023-06-14 18:01:49 +0300

    create project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./page-components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      xl: "1280px",
+      smOnly: { max: "767.98px" },
+      mdOnly: { min: "768px", max: "1279.98px" },
+      notXl: { max: "1279.98px" },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1.25rem",
+          sm: "1.25rem",
+          md: "2rem",
+          xl: "2rem",
+        },
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
       },
     },
+    colors: {
+      whiteBg: "#FFFFFF",
+      blueBg: "#F0F4FF",
+      blackBg: "#252525",
+      borderPrimary: "#CCCCCC",
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
