e1daadb17ef0fdce1320f00e83de76813872b428
Serhii Rybak <rybaksw@gmail.com>
2023-06-19 13:19:11 +0300

    add telegram bot

--- page-components/Form/Form.tsx
+++ page-components/Form/Form.tsx
@@ -14,8 +14,22 @@ const Form: FC = () => {
     formState: { errors },
   } = useForm<FormInputsType>();
 
-  const onSubmit: SubmitHandler<FormInputsType> = data => {
+  const onSubmit = async (data: FormInputsType) => {
     console.log(data);
+    const message = `Name: ${data.name}\nPhone: ${data.phone}\nEmail: ${data.email}`;
+    const response = await fetch(
+      `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_BOT_TOKEN}/sendMessage`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `chat_id=${process.env.NEXT_PUBLIC_GROUP_ID}&text=${encodeURIComponent(message)}`,
+      }
+    );
+
+    const responseData = await response.json();
+    console.log('responseData:', responseData);
 
     reset();
   };
@@ -28,7 +42,7 @@ const Form: FC = () => {
     >
       <h2 className="mb-8 text-[24px] font-bold leading-[0.95]">Оставить заявку</h2>
 
-      <div className="mb-4 flex flex-col gap-3">
+      <div className="flex flex-col gap-3 mb-4">
         <Input
           register={register('name', { ...fieldsParams.name })}
           placeholder="Вашe Имя"
