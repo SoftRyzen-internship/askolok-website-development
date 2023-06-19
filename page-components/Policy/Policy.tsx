79bf4a0ee4412e783a29bef500a04656229bc9e0
LeonidGHA <104501564+LeonidGHA@users.noreply.github.com>
2023-06-19 11:09:44 +0300

    modal, modal link, fix some sections

--- page-components/Policy/Policy.tsx
+++ page-components/Policy/Policy.tsx
@@ -7,14 +7,16 @@ import { policyText } from '@/public/materials/policy';
 const Policy: FC = () => {
   const { sections, text1, text2, text3, title, subtitle } = policyText;
   return (
-    <div className="container">
-      <h2 className=" mb-[60px] text-center text-[48px] font-medium leading-[0.63]">{title}</h2>
+    <>
+      <h2 className=" mb-[60px] text-center text-[24px] font-medium leading-[0.63] md:text-[32px] xl:text-[48px]">
+        {title}
+      </h2>
       <h3 className="mb-[60px] text-center text-[32px] leading-[0.93]">{subtitle}</h3>
-      <p className="mb-10 text-xl text-black">{text1}</p>
-      <p className="mb-10 text-xl text-black">{text2}</p>
-      <p className="mb-10 text-xl text-black">{text3}</p>
+      <p className="mb-10 text-sm text-black md:text-base xl:text-xl">{text1}</p>
+      <p className="mb-10 text-sm text-black md:text-base xl:text-xl">{text2}</p>
+      <p className="mb-10 text-sm text-black md:text-base xl:text-xl">{text3}</p>
       <PolicyList sections={sections} />
-    </div>
+    </>
   );
 };
 
