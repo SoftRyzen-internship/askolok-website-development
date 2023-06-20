73f6957eaa86472e10bcd43f5ff2a33252868946
Serhii Rybak <rybaksw@gmail.com>
2023-06-20 18:08:49 +0300

    fix: change img to svg (advantage section) put texts to json (advantages,services) delete double padding (service section)

--- components/common/AdvantageCard/AdvantageCard.props.ts
+++ components/common/AdvantageCard/AdvantageCard.props.ts
@@ -1,8 +1,4 @@
 export interface IAdvantageCardProps {
-  iconUrl:string;
-  alt:string;
   title: string;
   description: string;
-  color?: 'black' | 'white' | 'gradient';
-  className?: string;
 }
